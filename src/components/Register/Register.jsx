9adc89f00c022539202015fa75c5f6ecbe4fcfb3
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-05-20 12:23:32 +0600

    Create Register Section and Make Responsive

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -46,13 +46,13 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
 
                         </div>
                         <div className="form-control">
