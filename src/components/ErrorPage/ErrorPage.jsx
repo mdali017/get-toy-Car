fc98a771e24aaf826aa540924552ff46054ea9b5
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-05-20 12:31:23 +0600

    Create Error Route And Make responsive

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -5,16 +5,14 @@ import Lottie from "lottie-react";
 import errorPageImg from "../../assets/43391-404-error-page-not-found-confused-robot.json";
 
 const ErrorPage = () => {
-  const { error, status } = useRouteError()
+  const { error} = useRouteError()
   return (
     <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
-        {/* <FaceFrownIcon className='w-40 h-40 text-yellow-500' /> */}
         <div className='max-w-md text-center'>
           <h2 className='mb-8 font-extrabold text-9xl text-yellow-500'>
             <span className='sr-only'>Error</span>
             <Lottie animationData={errorPageImg} loop={true} />
-            {/* {status || 404} */}
           </h2>
           <p className='text-2xl font-semibold md:text-3xl text-red-800 mb-8'>
             {error?.message}
