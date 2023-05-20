f2351dcb8d2a804730db337f5fe3c8b0560098bc
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-05-20 12:27:28 +0600

    Create Extra Section Two And Make Responsive

--- src/components/ExtraSection/ExtraSection.jsx
+++ src/components/ExtraSection/ExtraSection.jsx
@@ -17,7 +17,7 @@ const ExtraSection = () => {
                 <div>
                     <img src="https://images.pexels.com/photos/981588/pexels-photo-981588.jpeg?auto=compress&cs=tinysrgb&w=600" className='w-36 h-28 mx-auto mt-5 mb-5 rounded-lg' alt="" />
                     <h2 className='font-bold text-2xl mb-5'>Toy Truck</h2>
-                    <p className='text-[#4F4F4F] mb-20 '>A toy truck is a miniature vehicle designed for play and imagination. It often resembles a real truck, complete with wheels, a chassis, and a bed for carrying objects. Children enjoy pushing, pulling, and racing toy trucks, creating adventures in their own imaginary worlds.</p>
+                    <p className='text-[#4F4F4F] mb-20 '>A toy truck is a miniature vehicle designeed for play and imagination. It often resembles a real truck, complete with wheels, a chassis, and a bed for carrying objects. Children enjoy pushing, pulling, and racing toy trucks, creating adventures in their own imaginary worlds.</p>
                 </div>
                 <div>
                     <img src="https://images.pexels.com/photos/15520639/pexels-photo-15520639/free-photo-of-close-up-of-a-toy-rocket.jpeg?auto=compress&cs=tinysrgb&w=600" className='w-36 h-28 mx-auto mt-5 mb-5 rounded-lg' alt="" />
